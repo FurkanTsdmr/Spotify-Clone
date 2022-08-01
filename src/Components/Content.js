fff2931f244ee0b8e25f0faa76e16ebf4dad1424
Furkan Taşdemir <furkantsdmr97@gmail.com>
2022-08-01 15:28:03 +0300

    adding-the-components

---
+++ src/Components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "Components/Navbar";
+
+
+function Content() {
+    return (
+        <main>
+            <Navbar />
+            content
+        </main>
+    )
+}
+
+export default Content
